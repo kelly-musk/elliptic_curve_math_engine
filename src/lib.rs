2820687b2b8a305a7f7dfa87e0c6997974f7a0c3
totdking <timileyindgreat02@gmail.com>
2025-12-06 13:03:55 +0100

    skeleton structure

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//!## Project 5: Elliptic Curve Math Engine
+//!### Implement 256-bit modular arithmetic for secp256k1:
+
+use std::ops::{Add, Mul, Sub, Div};
+use primitive_types::U256;
+
+
+// pub const P: U256 = U256::from(2_u128.pow(256)) - U256::from(2_u128.pow(32)) - U256::from(977);
+
+// pub struct 
